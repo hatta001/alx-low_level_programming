10343dc29446978f69093b7c590a1390ff727070
hatta001 <kevinjos321@gmail.com>
2022-06-04 08:43:33 -0700

    Task. 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,19 @@
 #include <stdio.h>
-{printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);}
+
+/**
+ *
+ *  * Description: main - Prints out a string to stdout.
+ *
+ *   * Return: 0 if success.
+ *
+ *    */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
