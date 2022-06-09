34f115f539973af51dc0027f4ddcd07b05d5fd4d
hatta001 <kevinjos321@gmail.com>
2022-06-09 07:34:00 -0700

    first commit

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/* main - Entry point
+ * Return: Always 0 (Success)
+ */
+int positive_or_negative(void)
+{
+	int j;
+
+	j = 0;
+	positive_or_negative(j);
+
+	return (0);
+}
