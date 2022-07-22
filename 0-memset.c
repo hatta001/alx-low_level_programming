e07579016e13244c9329a02a13fcb1d7f1efcd95
hatta001 <kevinjos321@gmail.com>
2022-07-22 08:09:02 -0700

    Bit manipulation
    C

---
+++ 0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - a function that fills memory with constant bytes
+ * @s: string
+ * @b: a character
+ * @n: an integer
+ * Return: a string
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		s[a] = b;
+
+	return (s);
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-C prgrogramming
+Bit-Manupilation
